fd71a611bad161a7c231486c6323a3d5a68301ff
arefe_hamidi <arefehamidi.guilan@gmail.com>
2023-08-02 11:40:48 +0330

    added theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,35 +9,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expensess',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
+                fontFamily: 'OpenSans',
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+      ),
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [];
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+        id: DateTime.now().toString(),
+        title: txTitle,
+        amount: txAmount,
+        date: DateTime.now());
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          child: NewTransaction(_addNewTransaction),
+          onTap: () {},
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text(
+          'Personal Expensess',
+          style: TextStyle(fontFamily: 'OpenSans'),
+        ),
+        actions: <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.add))],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.blue,
-                child: Text('CHART!'),
-                elevation: 5,
+        child: _userTransactions.isEmpty
+            ? Column(
+                children: [
+                  Text('No Transactions added yet!',
+                      style: Theme.of(context).textTheme.headline6),
+                ],
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    child: Card(
+                      color: Colors.blue,
+                      child: Text('CHART!'),
+                      elevation: 5,
+                    ),
+                  ),
+                  TransactionList(_userTransactions)
+                ],
               ),
-            ),
-            UserTransactions()
-          ],
-        ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => _startAddNewTransaction(context)),
     );
   }
 }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  final Function newTx;
+class NewTransaction extends StatefulWidget {
+  final Function addTx;
+  NewTransaction(this.addTx);
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
-  NewTransaction(this.newTx);
+  void submitDate() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+    widget.addTx(enteredTitle, enteredAmount);
+    Navigator.of(context).pop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -19,18 +34,18 @@ class NewTransaction extends StatelessWidget {
                   labelText: 'Title:',
                 ),
                 controller: titleController,
+                onSubmitted: (_) => submitDate,
               ),
               TextField(
                 decoration: InputDecoration(
                   labelText: 'amount:',
                 ),
                 controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => submitDate,
               ),
               TextButton(
-                onPressed: () {
-                  newTx(titleController.text,
-                      double.parse(amountController.text));
-                },
+                onPressed: submitDate,
                 child: Text('Add Transaction'),
               )
             ],
